9bc6798aef6a84c13dfd4873c8a74d939d8ed306
AtaS <atasasmaz@gmail.com>
2023-10-15 11:04:24 +0100

    add last deploy info

--- layout/footer.php
+++ layout/footer.php
@@ -2,10 +2,20 @@
 </div>
 
 <footer>
-    <p>Copyright &copy; 2023
+
+    <div style="display: flex;">
+        <div style="">Copyright &copy; 2023</div>
+        <div style="flex: 1; text-align: right;">
+            <a href="https://github.com/atas/ssg" target="_blank">
+                Last deploy: <?= date("Y-m-d") . " at " . date("h:ia"); ?>
+            </a>
+        </div>
+    </div>
+
     <div class="gdprInfo">
         <!-- TODO: Add GDPR info or a GDPR banner if using advanced analytics -->
-        This site respects your privacy with GDPR-compliant, self-hosted and anonymised analytics without using cookies and sharing data.
+        This site respects your privacy with GDPR-compliant, self-hosted and anonymised analytics without using cookies
+        and sharing data.
     </div>
 </footer>
 
